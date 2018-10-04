b22ab29443df1017e17b1525131442172b2e86c4
voxace <dave@voxace.com>
2018-10-04 13:26:13 +1000

    Added a few tasks

---
+++ 15-Functions/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _15_Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input two numbers
+            Console.Write("Enter number 1: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Enter number 2: ");
+            int y = int.Parse(Console.ReadLine());
+
+            // EXAMPLE: Use the function "AddNumbers" and print the result
+            int additionResult = AddNumbers(x, y);
+            Console.WriteLine($"{x} plus {y} equals {additionResult}");
+
+            // EXAMPLE 2: Using "AddNumbers" with the numbers 3 and 7 instead of the inputs
+            int additionResult2 = AddNumbers(3, 7);
+            Console.WriteLine($"3 plus 7 equals {additionResult2}");
+
+            // 1. Create a function called MultiplyNumbers that takes two integers
+            // 2. Create an integer variable called "multiplicationResult" and call the function using x and y
+
+
+            // 3. Output the result of the multiplication below
+
+
+            // 4. Create an integer variable called "multiplicationResult2" and call the function using 3 and 7
+
+
+            // 5. Output the result of the second multiplication below
+
+
+            // The "Wait" subroutine is called here
+            Wait();
+        }
+
+        // This functions takes two numbers, adds them and returns the answer
+        private static int AddNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        private static void Wait()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static void Questions()
+        {
+            // 1. What is a function?
+            // 
+            // 
+            // 2. What does a function do that a subroutine does not?
+            // 
+            // 
+        }
+
+        static void ExtensionTasks()
+        {
+            // 1. Try creating extra functions with parameters that do whatever you like
+            // 2. Experiment with different data types for the parameters and return types
+        }
+    }
+}
