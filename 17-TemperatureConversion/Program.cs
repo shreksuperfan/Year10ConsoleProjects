72bcfc5f706f57e322fe00e37540aff9be5f3bdd
shreksuperfan <109998345+shreksuperfan@users.noreply.github.com>
2022-09-16 11:51:01 +1000

    Update Program.cs

--- 17-TemperatureConversion/Program.cs
+++ 17-TemperatureConversion/Program.cs
@@ -6,17 +6,63 @@ namespace _17_TemperatureConversion
     {
         static void Main(string[] args)
         {
-            /* TASK:
-             * Implement a program that can convert from Fahrenheit to Celcius
-             * and from Celcius to Fahrenheit. The user chooses which to do.
-             * This program must make use of what we have learnt about functions. 
-             */
+            string choice = MenuSystem();
+
+            if (choice == "1")
+            {
+                double celcius = ConvertCelciusToFahrenheit();
+                Console.WriteLine($"{celcius.ToString("F2")} fahrenheit");
+
+            }
+            else if (choice == "2")
+            {
+                double fahrenheit = ConvertFahrenheitToCelcius();
+                Console.WriteLine($"{fahrenheit.ToString("F2")} celcius");
+
+            }
+            else
+            {
+                Console.WriteLine("Please Choose option 1 or 2");
+            }
 
 
+            Console.ReadLine();
 
         }
 
-        static void Pseudocode()
+        static double ConvertCelciusToFahrenheit()
+        {
+            double celcius = GetDouble();
+            return celcius = (celcius * 9 / 5) + 32;
+
+        }
+
+        static double ConvertFahrenheitToCelcius()
+        {
+            double fahrenheit = GetDouble();
+            return fahrenheit = ((fahrenheit - 32) * 5 / 9);
+
+        }
+        static double GetDouble()
+        {
+            Console.WriteLine("enter your number to convert");
+            return double.Parse(Console.ReadLine());
+        }
+
+
+        static string MenuSystem()
+        {
+            Console.WriteLine("Please Choose on of The Following:  ");
+            Console.WriteLine("1. Celcius to Fahrenheit");
+            Console.WriteLine("2. Fahrenheit to Celcius");
+            return Console.ReadLine();
+        }
+
+
+
+    
+
+    static void Pseudocode()
         {
             // Below is the pseudocode for this task to guide you
             // You may choose to implement this in a different way if you wish
